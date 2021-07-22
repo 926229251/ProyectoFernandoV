f03b2e7af7ae56b1f785c8b0e7f6929c01a068da
VictorManuelS99 <victor.albzzy@gmail.com>
2021-07-22 18:26:28 -0500

    fern '

---
+++ index.js
@@ -0,0 +1,44 @@
+function submitMensaje() {
+  window.alert("Mensaje enviado");
+}
+
+$(".fv-viajar__carousel1").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  auto:true,
+  arrows: true,
+  prevArrow: $(".arrow_l_1"),
+  nextArrow: $(".arrow_r_1"),
+});
+
+$(".fv-viajar__carousel2").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  auto:true,
+  arrows: true,
+  prevArrow: $(".arrow_l_2"),
+  nextArrow: $(".arrow_r_2"),
+});
+
+$(".fv-viajar__carousel3").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  auto:true,
+  arrows: true,
+  prevArrow: $(".arrow_l_3"),
+  nextArrow: $(".arrow_r_3"),
+});
+
+$(".fv-viajar__carousel4").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  auto:true,
+  arrows: true,
+  prevArrow: $(".arrow_l_4"),
+  nextArrow: $(".arrow_r_4"),
+});
+
